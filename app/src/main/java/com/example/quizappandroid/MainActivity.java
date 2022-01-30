0767dceb6598c0278db4ae51bdb850244ae558bd
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-01-30 16:24:30 +0700

    basic function

--- app/src/main/java/com/example/quizappandroid/MainActivity.java
+++ app/src/main/java/com/example/quizappandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.quizappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClickPlayBtn(View view){
+        TextView showname=findViewById(R.id.showname);
+        EditText inputname=findViewById(R.id.inputName);
+        showname.setText("Your name is: "+ inputname.getText().toString());
     }
 }
