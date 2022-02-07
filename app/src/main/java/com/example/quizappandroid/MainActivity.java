762691d3281068f0b68e09f65aec874d6c3126da
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-02-07 20:07:42 +0700

    change intent test

--- app/src/main/java/com/example/quizappandroid/MainActivity.java
+++ app/src/main/java/com/example/quizappandroid/MainActivity.java
@@ -2,22 +2,34 @@ package com.example.quizappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button playBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playBtn =(Button)findViewById(R.id.playButton);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText inputname=findViewById(R.id.inputName);
+                String welcome= "Hello "+ inputname.getText().toString();
+                Toast toast = Toast.makeText(MainActivity.this, welcome,  Toast.LENGTH_SHORT);
+                toast.show();
+                openQuizActivity();
+            }
+        });
     }
-    public void onClickPlayBtn(View view){
-        EditText inputname=findViewById(R.id.inputName);
-        String welcome= "Hello "+ inputname.getText().toString();
-        Toast toast = Toast.makeText(MainActivity.this, welcome,  Toast.LENGTH_SHORT);
-        toast.show();
+    public void openQuizActivity(){
+        Intent quizActivity = new Intent(this, QuizActivity.class);
+        startActivity(quizActivity);
     }
 }
