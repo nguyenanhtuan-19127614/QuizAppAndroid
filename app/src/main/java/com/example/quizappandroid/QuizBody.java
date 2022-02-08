ed84e5345f767a5724affc2c1969f735cf1e2ae7
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-02-08 19:17:15 +0700

    quiz class

---
+++ app/src/main/java/com/example/quizappandroid/QuizBody.java
@@ -0,0 +1,67 @@
+package com.example.quizappandroid;
+
+public class QuizBody {
+    private String question;
+    private String ans1;
+    private String ans2;
+    private String ans3;
+    private String ans4;
+    private String trueAns;
+
+    public QuizBody(String question, String ans1, String ans2, String ans3, String ans4, String trueAns) {
+        this.question = question;
+        this.ans1 = ans1;
+        this.ans2 = ans2;
+        this.ans3 = ans3;
+        this.ans4 = ans4;
+        this.trueAns = trueAns;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAns1() {
+        return ans1;
+    }
+
+    public void setAns1(String ans1) {
+        this.ans1 = ans1;
+    }
+
+    public String getAns2() {
+        return ans2;
+    }
+
+    public void setAns2(String ans2) {
+        this.ans2 = ans2;
+    }
+
+    public String getAns3() {
+        return ans3;
+    }
+
+    public void setAns3(String ans3) {
+        this.ans3 = ans3;
+    }
+
+    public String getAns4() {
+        return ans4;
+    }
+
+    public void setAns4(String ans4) {
+        this.ans4 = ans4;
+    }
+
+    public String getTrueAns() {
+        return trueAns;
+    }
+
+    public void setTrueAns(String trueAns) {
+        this.trueAns = trueAns;
+    }
+}
