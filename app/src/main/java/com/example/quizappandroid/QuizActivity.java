f4fa6c84466e4d4b1552fa8c712b5742e074ee5b
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-02-08 19:37:03 +0700

    set quiz on phone scene

--- app/src/main/java/com/example/quizappandroid/QuizActivity.java
+++ app/src/main/java/com/example/quizappandroid/QuizActivity.java
@@ -27,13 +27,25 @@ public class QuizActivity extends AppCompatActivity {
         option4=findViewById(R.id.opt4);
         quizList=new ArrayList<>();
         random=new Random();
+        getQuiz(quizList);
+        curPos=random.nextInt(quizList.size());
+        setView(curPos);
     }
-    private void GetQuiz(ArrayList<QuizBody> quizList){
+    private void getQuiz(ArrayList<QuizBody> quizList){
         quizList.add(new QuizBody("1+1","2","3","4","5","2"));
         quizList.add(new QuizBody("1+2","2","3","4","5","3"));
         quizList.add(new QuizBody("1+3","2","3","4","5","4"));
         quizList.add(new QuizBody("1+4","2","3","4","5","5"));
         quizList.add(new QuizBody("1+5","2","6","4","5","6"));
 
+    }
+    private void setView(int curPos){
+        numberQuestionView.setText("Number Question:" + this.numQuest+ "/" + this.quizList.size());
+        scoreView.setText("Score:" + curScore);
+        questionName.setText(quizList.get(curPos).getQuestion());
+        option1.setText(quizList.get(curPos).getAns1());
+        option2.setText(quizList.get(curPos).getAns2());
+        option3.setText(quizList.get(curPos).getAns3());
+        option4.setText(quizList.get(curPos).getAns4());
     }
 }
